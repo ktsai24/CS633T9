78612a15a32dc944a6cf44cbd1c427c4a9c879d0
Brian Ray Xu <33497224+brianrayxu@users.noreply.github.com>
2023-02-25 10:46:47 -0500

    Update Flights.js

--- Backend/Flights.js
+++ Backend/Flights.js
@@ -1,34 +1,50 @@
 // Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-import {local} from 'wix-storage'
+import { local } from 'wix-storage'
 
 local.setItem("ForH", "F")
 
 $w.onReady(function () {
-	console.log("Hello world!");
+    console.log("Hello world!");
 });
+
+$w("#input7").onCustomValidation( (value, reject) => {
+  if(!isValidDestination(value)) {
+    reject("Invalid Format");
+  }
+} );
+
+$w("#input6").onCustomValidation( (value, reject) => {
+  if(!isValidDestination(value)) {
+    reject("Invalid Format");
+  }
+} );
 
 /**
  *	Adds an event handler that fires when a visitor submits a Wix Form and it is successfully received by the server.
  */
 export function multiStepForm1_wixFormSubmitted(fields) {
-	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
-	// Add your code for this event here: 
-	console.log(fields)
-	console.log(fields.fields[2].fieldValue)
-	let leavingFromQuery = fields.fields[0].fieldValue;
-	let goingToQuery = fields.fields[1].fieldValue;
-	let departingDate =JSON.stringify(fields.fields[2].fieldValue);
-	let returningDate = JSON.stringify(fields.fields[3].fieldValue);
-	
-	let departingDate2 = departingDate.substring(1,11)
-	let returningDate2 = returningDate.substring(1,11)
+    // This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+    // Add your code for this event here: 
+    console.log(fields)
+    console.log(fields.fields[2].fieldValue)
+    let leavingFromQuery = fields.fields[0].fieldValue;
 
-	local.setItem("lfqkey", leavingFromQuery);
-	local.setItem("gtkey", goingToQuery);
-	local.setItem("ddatekey", departingDate2);
-	local.setItem("rdatekey", returningDate2);
+    let goingToQuery = fields.fields[1].fieldValue;
+    let departingDate = JSON.stringify(fields.fields[2].fieldValue);
+    let returningDate = JSON.stringify(fields.fields[3].fieldValue);
 
+    let departingDate2 = departingDate.substring(1, 11)
+    let returningDate2 = returningDate.substring(1, 11)
 
-	console.log("Form Submitted")
-	console.log("Flight from " + leavingFromQuery + " going to " + goingToQuery + " on " + departingDate + " and retuning on " + returningDate )
+    local.setItem("lfqkey", leavingFromQuery);
+    local.setItem("gtkey", goingToQuery);
+    local.setItem("ddatekey", departingDate2);
+    local.setItem("rdatekey", returningDate2);
+
+    console.log("Form Submitted")
+    console.log("Flight from " + leavingFromQuery + " going to " + goingToQuery + " on " + departingDate + " and retuning on " + returningDate)
+}
+export function isValidDestination(destination) {
+    const destinationRegex = /^[a-zA-Z]+,\s*[a-zA-Z]+$/;
+    return destinationRegex.test(destination);
 }
